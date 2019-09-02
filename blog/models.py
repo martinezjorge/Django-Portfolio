d43f6dcd783bcc2520a7e8da281658bbb04647f2
Jorge <martinezjorge273@gmail.com>
2019-09-01 21:56:02 -0700

    Added bootstrap and postgress db

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    date = models.DateTimeField()
+    post = models.CharField(max_length=500)
+    image = models.ImageField(upload_to='images/')
