a8e19456de84d2386f226eabf7c7bd005298cb56
Jorge <martinezjorge273@gmail.com>
2019-09-02 01:26:53 -0700

    Finished job and blog app functionality

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,15 @@ class Blog(models.Model):
     date = models.DateTimeField()
     post = models.CharField(max_length=500)
     image = models.ImageField(upload_to='images/')
+
+    # this function posts the title in the admin page
+    def __str__(self):
+        return self.title
+
+    # this limits the number of characters that show from a post
+    def summary(self):
+        return self.post[:100]
+
+    # show the date but not the time
+    def date_pretty(self):
+        return self.date.strftime('%b %e %Y')
